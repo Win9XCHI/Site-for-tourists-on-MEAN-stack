0acc25e4e8d59c19e3e3f333ecd0c1f031569657
Win9XCHI <nikulicov@gmail.com>
2019-08-31 21:34:44 +0300

    add date and currency in calendar page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -5,14 +5,80 @@ module.exports.index = function(req, res) {
 module.exports.HomePage = function(req, res) {
     var Object = [{
         title: 'Від Попа до Попа',
+        expedition: false,
         message: 'Щось про похід',
-        date: [new Date(2019, 6, 20), new Date(2019, 7, 20)],
-        money: 2000,
+        date: [new Date(2019, 6, 2), new Date(2019, 7, 20)],
+        money: 3000,
         Currency: 'UAH',
         Foto: 'images/2.jpg',
         quantityDay: 7,
         km: 54,
         Complexity: 2,
+        Region: 'Карпати',
+        RouteShort: 'Рахів - Ділове - вдсп. Ялинський - г. Піп Іван Мармароський - г. Межипотоки - г. Стіг - г. Вихід - г. Піп Іван Чорногірський - Дземброня - Ворохта',
+        Information: '',
+        Payment: '',
+        Weather: '',
+        Documents: '',
+        Accommodation: '',
+        Food: ['', ''],
+        equipment: ['', ''],
+        RouteLong: [{
+            day: 1,
+            info: 'QWE',
+            foto: 'rftg.jpg'
+                   }, 
+                   {
+            day: 2,
+            info: 'RTY',
+            foto: 'rftg.jpg'
+                   }],
+        Rewiews: {}
+    },
+                 {
+        title: 'Польські Високі Татри',
+        expedition: true,
+        message: 'Щось про похід',
+        date: [new Date(2019, 6, 29), new Date(2020, 7, 20)],
+        money: 150,
+        Currency: 'EUR',
+        Foto: 'images/PTatrs2.jpg',
+        quantityDay: 6,
+        km: 54,
+        Complexity: 3,
+        Region: 'Татри',             
+        RouteShort: 'Закопане - г. Гевонт - г. Каспрови Верх - пер. Заврат - Долина п\'яти ставів - г. Шпігласов верх - оз. Морське Око - г. Риси - Закопане',
+        Information: '',
+        Payment: '',
+        Weather: '',
+        Documents: '',
+        Accommodation: '',
+        Food: ['', ''],
+        equipment: ['', ''],
+        RouteLong: [{
+            day: 1,
+            info: 'QWE',
+            foto: 'rftg.jpg'
+                   }, 
+                   {
+            day: 2,
+            info: 'RTY',
+            foto: 'rftg.jpg'
+                   }],
+        Rewiews: {}
+    },
+                 {
+        title: 'Похід Чорногорою',
+        expedition: false,
+        message: 'Щось про похід',
+        date: [new Date(2019, 4, 20)],
+        money: 2000,
+        Currency: 'UAH',
+        Foto: 'images/3.jpg',
+        quantityDay: 4,
+        km: 54,
+        Complexity: 2,
+        Region: 'Карпати',
         RouteShort: 'Рахів - Ділове - вдсп. Ялинський - г. Піп Іван Мармароський - г. Межипотоки - г. Стіг - г. Вихід - г. Піп Іван Чорногірський - Дземброня - Ворохта',
         Information: '',
         Payment: '',
@@ -54,12 +120,24 @@ module.exports.HomePage = function(req, res) {
             }
         }
     }
-    //console.log(result[0].MasObject);
     
-    /*var result = {
-        MasObject: Object,
-        CalendarItem: Item
-    };*/
+    var k = 0, rab;
+    
+    while (k == 0) {
+        k = 1;
+        for (i = 0; i < result.length - 1; i++) {
+            if ((result[i].month > result[i + 1].month && result[i].year <= result[i + 1].year) || result[i].year > result[i + 1].year) {      
+                k = 0;
+                rab = result[i];
+                result[i] = result[i + 1];
+                result[i + 1] = rab;            
+            }
+            
+        }
+    }
+    
+    
+    
     
     res.render('Calendar', {resultO: result});
 };
